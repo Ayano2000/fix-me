52efd0b9bd2e1329b5525b1f86db9fae87d702a5
Aratayano00 <aratayano00@gmail.com>
2020-09-08 19:51:20 +0200

    working on the broker class in market

--- market/src/main/java/market/model/brokers/Broker.java
+++ market/src/main/java/market/model/brokers/Broker.java
@@ -4,14 +4,24 @@ public class Broker implements Interface.Broker
 {
     private String ID;
     private int balance;
-    // need to figure out how to keep track of stock owned
-    // maybe change the id if the instruments to make it a
-    // counter like we had in avaj-launcher? :)
+    private string[5][2] stock;
+    // will need to convert int values for stock to strings for this array.
 
     public Broker(String brokerID, int brokerBalance)
     {
         ID = brokerID;
         balance = brokerBalance;
+        // this is ugly but we can clean it later
+        stock[0][0] = "XSD01";
+        stock[0][1] = "0";
+        stock[0][0] = "RSD01";
+        stock[0][1] = "0";
+        stock[0][0] = "RMD01";
+        stock[0][1] = "0";
+        stock[0][0] = "RLD01";
+        stock[0][1] = "0";
+        stock[0][0] = "XLD01";
+        stock[0][1] = "0";
     }
 
     public String getID() {
@@ -30,6 +40,18 @@ public class Broker implements Interface.Broker
     public void setBalance(int value) {
         balance = value;
         return;
+    }
 
+    public void addStock(string id, int amount)
+    {
+        if (id.equals("XSD01"))
+        {
+            int newAmount = Integer.parseInt(stock[0][1]) + amount;
+            stock[0][1] = newAmount.toString();
+        }
+        if (id.equals("RSD01"))
+        if (id.equals("RMD01"))
+        if (id.equals("RLD01"))
+        if (id.equals("XLD01"))
     }
 }
