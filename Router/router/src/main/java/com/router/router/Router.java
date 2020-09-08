1d43ba7b7f720093bbdc0f631d019a15d2d1581e
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-09-08 22:29:54 +0200

    working on id generation + communication to and from broker

--- Router/router/src/main/java/com/router/router/Router.java
+++ Router/router/src/main/java/com/router/router/Router.java
@@ -14,12 +14,15 @@ public class Router {
 			ServerSocket brokerSocket = new ServerSocket(5000);
 			ServerSocket marketSocket = new ServerSocket(5001);
 			// waits for market to connect to server on port 5001, accepts connection
-			Socket marketClientSocket = marketSocket.accept();
+			Socket marketClientSocket = null;
 			// infinitely listens for connections on port 5000, creating new ServerThread for each connection
 			while (true) {
 				try {
+//					if (marketClientSocket == null) {
+//						marketClientSocket = marketSocket.accept();
+//					}
 					System.out.println("gon do this shit now");
-					new ServerThread(brokerSocket.accept()).start();
+					new ServerThread(brokerSocket.accept(), marketClientSocket).start();
 				} catch (IOException e) {
 					throw e;
 				}
--- Router/router/src/main/java/com/router/router/ServerThread.java
+++ Router/router/src/main/java/com/router/router/ServerThread.java
@@ -6,18 +6,26 @@ import java.util.*;
 
 public class ServerThread extends Thread {
 	protected Socket brokerSocket = null;
+	protected Socket marketSocket = null;
 	static final ArrayList<String> marketIDs = new ArrayList<String>();
 	static final ArrayList<String> brokerIDs = new ArrayList<String>();
 
-	public ServerThread(Socket brokerSocket) {
-		System.out.println("port" + brokerSocket.getPort());
+	public ServerThread(Socket brokerSocket, Socket marketSocket) {
+//		System.out.println(brokerSocket.getPort());
 		this.brokerSocket = brokerSocket;
+		this.marketSocket = marketSocket;
 	}
 
 	public void run() {
 		try {
-			PrintWriter output = new PrintWriter(brokerSocket.getOutputStream(), true);
+			PrintWriter output = new PrintWriter(marketSocket.getOutputStream(), true);
 			BufferedReader input = new BufferedReader(new InputStreamReader(brokerSocket.getInputStream()));
+			/**
+			 * TODO:
+			 * - create random generated broker id // will this be safe? / /create with brokerSocket.getPort()
+			 * - write to output stream: brokerid
+			 */
+			output.println("helloyesthisisid");
 			String request, response;
 			while (true) {
 				try {
@@ -26,15 +34,22 @@ public class ServerThread extends Thread {
 						break;
 					else
 						System.out.println(request);
+					// TODO: validate request
 					output.println(request);
 				} catch (IOException e) {
 					throw e;
 				}
 			}
 			brokerSocket.close();
+			marketSocket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 			return;
 		}
 	}
+
+	private void generateId() {
+
+	}
+
 }
