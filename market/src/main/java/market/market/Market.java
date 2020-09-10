0b454371b38baaf7f6acbd46896ff05ae71e2214
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-09-10 21:27:48 +0200

    just writing to output stream for testing purposes

--- market/src/main/java/market/market/Market.java
+++ market/src/main/java/market/market/Market.java
@@ -2,31 +2,39 @@ package main.java.market.market;
 
 import main.java.market.controllers.MarketController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Market {
     public static void main(String[] args) {
         // now to handle the communication with the market
         MarketController marketController = new MarketController();
 
-//        try (Socket socket = new Socket("localhost", 5001)) {
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-//            BufferedReader standardInput = new BufferedReader(new InputStreamReader(System.in));
-//
-//            String received;
-//            String toSend;
-//
-//            while (true)
-//            {
-//                received = reader.readLine();
-//                System.out.println(received);
-//                // todo
-////                toSend = marketController.evaluate();
-//            }
-//
-//        } catch (UnknownHostException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        try (Socket socket = new Socket("localhost", 5001)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader standardInput = new BufferedReader(new InputStreamReader(System.in));
+
+            String received;
+            String toSend;
+
+            while (true)
+            {
+                received = reader.readLine();
+                System.out.println(received);
+                writer.println("i heard you");
+                // todo
+//                toSend = marketController.evaluate();
+            }
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
