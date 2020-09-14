a2adcaba8bf94e3e910d369f847f7f6a97f5960e
Rigardt Engelbrecht <45070199+RWEngelbrecht@users.noreply.github.com>
2020-09-14 14:41:39 +0200

    Update Market.java

--- market/src/main/java/market/market/Market.java
+++ market/src/main/java/market/market/Market.java
@@ -12,29 +12,29 @@ import java.net.UnknownHostException;
 public class Market {
     public static void main(String[] args) {
         // now to handle the communication with the market
-        MarketController marketController = new MarketController();
+//         MarketController marketController = new MarketController();
 
-        try (Socket socket = new Socket("localhost", 5001)) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader standardInput = new BufferedReader(new InputStreamReader(System.in));
+//         try (Socket socket = new Socket("localhost", 5001)) {
+//             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+//             BufferedReader standardInput = new BufferedReader(new InputStreamReader(System.in));
 
-            String received;
-            String toSend;
+//             String received;
+//             String toSend;
 
-            while (true)
-            {
-                received = reader.readLine();
-                System.out.println(received);
-                writer.println("i heard you");
-                // todo
-//                toSend = marketController.evaluate();
-            }
+//             while (true)
+//             {
+//                 received = reader.readLine();
+//                 System.out.println(received);
+//                 writer.println("i heard you");
+//                 // todo
+// //                toSend = marketController.evaluate();
+//             }
 
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//         } catch (UnknownHostException e) {
+//             e.printStackTrace();
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
     }
 }
