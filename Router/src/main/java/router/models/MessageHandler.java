853bc8ffc0ea03d4cd67ac925128dd3e2fc86847
RWEngelbrecht <rigardt@brechtburg.co.za>
2020-09-15 09:25:54 +0200

    only validates checksum for now

--- Router/src/main/java/router/models/MessageHandler.java
+++ Router/src/main/java/router/models/MessageHandler.java
@@ -6,48 +6,23 @@ import java.security.NoSuchAlgorithmException;
 public class MessageHandler {
     public MessageHandler() {}
 
-    public String parse(String message, String brokerID) {
-        String parsedMessage = brokerID;
-        String checksum;
-        parsedMessage += "|"+message.replaceAll(" ", "|");
+    public boolean validateChecksum(String message) {
+
+        int receivedChecksum = Integer.parseInt(message.split("\\|")[4]);
+        String originalMessage = message.substring(0, message.length() - ((int)(Math.log10(receivedChecksum)) + 2));
+        int checksum = 0;
+
+        for (int j = 0; j < originalMessage.length(); j++) {
+            int temp = (int) (Math.floor(Math.log(originalMessage.charAt(j)) / Math.log(2))) + 1;
+            checksum += ((1 << temp) - 1) ^ originalMessage.charAt(j);
+        }
         try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            StringBuilder sb = new StringBuilder();
-            md.update(parsedMessage.getBytes());
-            byte[] bytes = md.digest();
-
-            for (byte aByte : bytes) {
-                sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
-            }
-            checksum = sb.toString();
-            parsedMessage += "|"+checksum;
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+            if (checksum == receivedChecksum)
+                return true;
+        } catch (NumberFormatException e) {
+            System.out.println("Something went wrong: "+e);
         }
-        return parsedMessage;
-    }
 
-    public boolean validate(String message) {
-        String[] msgPieces = message.split(" ");
-        if (msgPieces.length == 3) {
-            if ((msgPieces[0].equalsIgnoreCase("buy") || msgPieces[0].equalsIgnoreCase("sell"))
-                && msgPieces[1].length() > 0 && msgPieces[2].length() > 0) {
-                try {
-                    Integer.parseInt(msgPieces[2]);
-                    return true;
-                } catch (NumberFormatException e) {
-                    System.out.println("Please format your message properly and try again.");
-                }
-            }
-        }
-        return false;
-    }
-
-    public boolean validateMarketMessage(String message) {
-        String[] msgPieces = message.split("\\|");
-        if (msgPieces.length == 5) {
-            // TODO: validate msg based on checksum
-        }
         return false;
     }
 }
