9206ac6be46334921e06ff29b0f41a5fd64dc852
Aratayano00 <aratayano00@gmail.com>
2020-09-18 13:14:21 +0200

    try this

--- market/src/main/java/market/market/Market.java
+++ market/src/main/java/market/market/Market.java
@@ -11,30 +11,31 @@ import java.net.UnknownHostException;
 
 public class Market {
     public static void main(String[] args) {
-        // now to handle the communication with the market
-//         MarketController marketController = new MarketController();
+//         now to handle the communication with the market
+         MarketController marketController = new MarketController();
 
-//         try (Socket socket = new Socket("localhost", 5001)) {
-//             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-//             BufferedReader standardInput = new BufferedReader(new InputStreamReader(System.in));
+         try (Socket socket = new Socket("localhost", 5001)) {
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader standardInput = new BufferedReader(new InputStreamReader(System.in));
 
-//             String received;
-//             String toSend;
+             String received;
+             String toSend;
 
-//             while (true)
-//             {
-//                 received = reader.readLine();
-//                 System.out.println(received);
-//                 writer.println("i heard you");
-//                 // todo
-// //                toSend = marketController.evaluate();
-//             }
+             while (true)
+             {
+                 received = reader.readLine();
+                 System.out.println(received);
+                 writer.println("i heard you");
+                 toSend = marketController.parseAndEvaluate(received);
+                 System.out.println(toSend);
 
-//         } catch (UnknownHostException e) {
-//             e.printStackTrace();
-//         } catch (IOException e) {
-//             e.printStackTrace();
-//         }
+             }
+
+         } catch (UnknownHostException e) {
+             e.printStackTrace();
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
     }
 }
